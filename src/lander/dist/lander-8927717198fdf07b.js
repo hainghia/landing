9e102abbb5a7e2227c6c01b141537df3ce6918d0
Hai Nghia <hohainghia19@gmail.com>
2023-06-05 07:42:06 +0700

    lander 2

--- src/lander/dist/lander-8927717198fdf07b.js
+++ src/lander/dist/lander-8927717198fdf07b.js
@@ -944,7 +944,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('lander-8ae3109c65bab9c4_bg.wasm', import.meta.url);
+        input = new URL('lander-8927717198fdf07b_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
